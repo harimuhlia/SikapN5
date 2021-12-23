eb865467dfa6fc314f36017b1b0ed385d834ed3f
harimuhlia <harimuhlia@gmail.com>
2021-12-23 12:54:06 +0700

    Edit Sedikit Untuk Persiapan Demo, Merapihkan Fomr dan Tampilan Luar

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -42,7 +42,7 @@
   <header id="header" class="fixed-top ">
     <div class="container d-flex align-items-center">
 
-      <h1 class="logo me-auto"><a href="index.html">Arsha</a></h1>
+      <h1 class="logo me-auto"><a href="index.html">SIKAP N5</a></h1>
       <!-- Uncomment below if you prefer to use an image logo -->
       <!-- <a href="index.html" class="logo me-auto"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
 
@@ -85,8 +85,8 @@
     <div class="container">
       <div class="row">
         <div class="col-lg-6 d-flex flex-column justify-content-center pt-4 pt-lg-0 order-2 order-lg-1" data-aos="fade-up" data-aos-delay="200">
-          <h1>Better Solutions For Your Business</h1>
-          <h2>We are team of talented designers making websites with Bootstrap</h2>
+          <h1>Selamat Datang Di SIKAP N5</h1>
+          <h2>Sistem Informasi Kinerja Aparatur, SMK Negeri 5 Kabupaten Tangerang</h2>
           <div class="d-flex justify-content-center justify-content-lg-start">
             <a href="#about" class="btn-get-started scrollto">Get Started</a>
             <a href="https://www.youtube.com/watch?v=jDDaplaOz7Q" class="glightbox btn-watch-video"><i class="bi bi-play-circle"></i><span>Watch Video</span></a>
@@ -138,7 +138,7 @@
     </section><!-- End Cliens Section -->
 
     <!-- ======= About Us Section ======= -->
-    <section id="about" class="about">
+    {{-- <section id="about" class="about">
       <div class="container" data-aos="fade-up">
 
         <div class="section-title">
@@ -168,7 +168,8 @@
         </div>
 
       </div>
-    </section><!-- End About Us Section -->
+    </section> --}}
+    <!-- End About Us Section -->
 {{-- 
     <!-- ======= Why Us Section ======= -->
     <section id="why-us" class="why-us section-bg">
